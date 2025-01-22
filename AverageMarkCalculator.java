fbac05d00eb307b76329a9a905f76a2ac6163c34
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2025-01-22 22:33:49 +0530

    found the average

--- AverageMarkCalculator.java
+++ AverageMarkCalculator.java
@@ -1,13 +1,10 @@
 public class AverageMarkCalculator {
     public static void main(String[] args) {
-
         int maths = 94;
         int physics = 95;
         int chemistry = 96;
-
         int totalMarks = maths + physics + chemistry;
         double averageMarks = totalMarks / 3.0;
-
         System.out.printf("Sam’s average mark in PCM is %.2f%n", averageMarks);
     }
 }
