ccc9dc49b73b2806f8879e722183ac6e09ecc1e9
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2025-01-25 22:19:34 +0530

    frequency

--- jan23/DigitFrequency.java
+++ jan23/DigitFrequency.java
@@ -1,5 +1,7 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class DigitFrequency {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
@@ -12,13 +14,16 @@ public class DigitFrequency {
             System.out.println("Digit " + i + ": " + frequency[i]);
         }
     }
+
     public static int[] findDigitFrequencies(int number) {
         int[] frequency = new int[10];
+
         while (number > 0) {
             int digit = number % 10;
             frequency[digit]++;
             number /= 10;
         }
+
         return frequency;
     }
 }
